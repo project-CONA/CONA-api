91479653a924fcb8689f81b8413fd5215c338663
Jihyun <bjih1999@naver.com>
2021-07-28 22:53:13 +0900

    feat: web config TODO 정리

---
+++ src/main/java/com/cona/projectcona/config/WebSecurityConfig.java
@@ -0,0 +1,13 @@
+package com.cona.projectcona.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //TODO 인증 절차 재정의
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//    }
+}
