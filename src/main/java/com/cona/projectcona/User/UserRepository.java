5c74e3b80e6e36fe88ed0c7d0633444215747fdc
jaehee <vito822@naver.com>
2021-08-25 17:59:59 +0900

    feat: user 엔티티

---
+++ src/main/java/com/cona/projectcona/User/UserRepository.java
@@ -0,0 +1,14 @@
+package com.cona.projectcona.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+/**
+ * 회원 저장소 인터페이스
+ * 1. 회원 저장 2. 회원 정보 수정 3. 아이디로 조회 4. 닉네임으로 조회 5.팔로우
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    Object save(User user);
+    Optional<User> findById(Long userId);
+}
---
+++ src/main/java/com/cona/projectcona/User/UserService.java
@@ -0,0 +1,13 @@
+package com.cona.projectcona.User;
+
+/**
+ * 1. 회원가입 2. 로그인 3. 로그아웃 4. 회원 정보 수정 5. 회원 검색 6.팔로우
+ */
+public interface UserService {
+    User join();
+    User login();
+    User logout();
+    User editUser();
+    User searchUser(Long searchByNickname);
+    User followUser(Long followerId, Long followingId);
+}
